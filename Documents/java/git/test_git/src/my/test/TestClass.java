df728675b7af05bb2f2e5f95dc69631a39a5e6d4
btvmds <btvmds@gmail.com>
2016-04-24 19:45:57 +0300

    commit1

---
+++ Documents/java/git/test_git/src/my/test/TestClass.java
@@ -0,0 +1,13 @@
+package my.test;
+
+/**
+ * Created by tbut on 24.04.16.
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+
+}
